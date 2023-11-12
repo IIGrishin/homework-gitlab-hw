c78e791b8066e5e22e0c4821a3d43f90b6774b6e
IIGrishin <iig.grishin@yandex.ru>
2023-11-12 10:50:42 +0000

    load_balancer

---
+++ terraform/load_balancer/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+    token     = ""
+    cloud_id  = "b1g3helsfp5tem1figin"
+    folder_id = "b1gkce6u9fr57vrgegs5"
+    zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  count = 2
+  name = "terraform1${count.index}"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8pf6624ff60n2pa1qk"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("/home/iigrishin/key.txt")}"
+  }
+
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_lb_target_group" "foo" {
+  name      = "my-target-group"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    address   = "192.168.10.3"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+    address   = "192.168.10.33"
+  }
+
+}
+
+resource "yandex_lb_network_load_balancer" "foo" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.foo.id}"
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/ping"
+      }
+    }
+  }
+}
